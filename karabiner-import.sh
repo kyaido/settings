1e792c9321963550452c03e26fe9c8029913a19d
kyaido <kamehameha1985@gmail.com>
2016-03-04 18:31:23 +0900

    edit karabiner

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -8,6 +8,8 @@ $cli set remap.jis_command2eisuukana_prefer_command 1
 /bin/echo -n .
 $cli set repeat.initial_wait 416
 /bin/echo -n .
+$cli set remap.fnletter_to_ctrlletter 1
+/bin/echo -n .
 $cli set option.vimode_control_hjkl 1
 /bin/echo -n .
 $cli set private.app_chrome_command_to_ctru 1
